bf587e92dcd1c4a1fa21aa160edac0070892d066
sl2 <dimension.sf@gmail.com>
2014-12-28 19:28:17 +0900

    Fix configuration of app icons

--- gulpfile.js
+++ gulpfile.js
@@ -8,12 +8,27 @@ var paths = {
     images:'src/images*.png'
 };
 
+// base size of iOSX s.t. X>=7
+var size = {
+    iPhone:{
+        settings:29,
+        spotlight:20,
+        app:30,
+        portrait:256
+    }
+}
+
+// output size for iOSX s.t. X>=7
 var icons = [
-    {size:58,   name:"Settings"},
-    {size:80,   name:"Spotlight"},
-    {size:120,  name:"App"},
-    {size:512,  name:"iPhonePortrait2x"},
-    {size:1024, name:"iPhonePortrait4x"}
+    {size:size.iPhone.settings*1,   name:"Settings1x"},
+    {size:size.iPhone.settings*2,   name:"Settings2x"},
+    {size:size.iPhone.settings*3,   name:"Settings3x"},
+    {size:size.iPhone.spotlight*2,  name:"Spotlight2x"},
+    {size:size.iPhone.spotlight*3,  name:"Spotlight3x"},
+    {size:size.iPhone.app*2,        name:"App2x"},
+    {size:size.iPhone.app*3,        name:"App3x"},
+    {size:size.iPhone.portrait*2,   name:"Portrait2x"},
+    {size:size.iPhone.portrait*4,   name:"Portrait4x"}
 ];
 
 var images = [
