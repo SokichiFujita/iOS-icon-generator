857c7fb6e2eef018c04e8ac5729540393135a46f
sl2 <dimension.sf@gmail.com>
2014-12-28 18:27:54 +0900

    Add app icons generator

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp');
+var rename = require('gulp-rename');
+var foreach = require('gulp-foreach');
+var gm = require('gulp-gm');
+
+var paths = {
+    appicons:'src/appicons*.png',
+    images:'src/images*.png'
+};
+
+var icons = [
+    {size:58,   name:"Settings"},
+    {size:80,   name:"Spotlight"},
+    {size:120,  name:"App"},
+    {size:512,  name:"iPhonePortrait2x"},
+    {size:1024, name:"iPhonePortrait4x"}
+];
+
+gulp.task('default', function () {
+    icons.forEach(function(icon){
+        gulp.src('./src/appicons/*.png')
+            .pipe(foreach(function(stream, file){ 
+                return stream
+                .pipe(gm(function(src) {
+                    return src.resize(icon.size, icon.size); 
+                 }))
+                .pipe(rename(function(path){
+                    path.basename = 
+                        path.basename + "_" + 
+                        icon.size + "x" + icon.size + "_" + 
+                        icon.name
+                 }))
+             }))
+            .pipe(gulp.dest('dist/appicons'));
+    });
+});
+
+
+
+
+
